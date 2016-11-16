94d81dcd0d9b22c5c74de5fe23535ef52d88f077
Rachel Johnson <rachelelise527@gmail.com>
2016-11-16 16:31:25 -0700

    first commit

---
+++ Default.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    <form id="form1" runat="server">
+        Image Path: <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><br />
+        Alt Text: <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox><br />
+        userID: <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox><br />
+        <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="Button1_Click" />
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+<asp:Button ID="btnUpload" runat="server" Text="Upload" OnClick="Upload" />
+<hr />
+<asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false" ShowHeader="false">
+    <Columns>
+        <asp:BoundField DataField="Text" />
+        <asp:ImageField DataImageUrlField="Value" ControlStyle-Height="100" ControlStyle-Width="100" />
+    </Columns>
+</asp:GridView>
+    <div>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.IO;
+
+public partial class Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string name = Request.QueryString["name"];
+        string email = Request.QueryString["email"];
+        Label1.Text = name + " " + email;
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            string connectionString = "uid=guest;server=192.168.12.122;port=3306;database=it210b;password=guest;";
+            string Query = "INSERT INTO images (imagePath,altText,userId) VALUES('" + TextBox1.Text + "','" + TextBox2.Text + "'," + TextBox3.Text + ")";
+            MySqlConnection MyConn2 = new MySqlConnection(connectionString);
+            MySqlCommand MyCommand2 = new MySqlCommand(Query, MyConn2);
+            MyConn2.Open();
+            MySqlCommand command = new MySqlCommand(Query, MyConn2);
+            command.ExecuteNonQuery();
+            MyConn2.Close();
+        }
+        catch (MySqlException ex)
+        {
+            int errorcode = ex.Number;
+            Label1.Text = ex.Message;
+        }
+    }
+    protected void Upload(object sender, EventArgs e)
+    {
+        if (FileUpload1.HasFile)
+        {
+            //string fileName = Path.GetFileName(FileUpload1.PostedFile.FileName);
+            string fileName = string.Format(@"{0}meme.jpeg", Guid.NewGuid());
+            FileUpload1.PostedFile.SaveAs(Server.MapPath("~/Images/") + fileName);
+            Response.Redirect(Request.Url.AbsoluteUri);
+        }
+    }
+}
